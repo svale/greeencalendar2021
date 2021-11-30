5293e657636621860edd4d0eb398914d2e90994a
Svale Fossåskaret <svaleaf@gmail.com>
2021-11-30 09:50:17 +0100

    Update config for gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,10 +2,19 @@ import preprocess from 'svelte-preprocess';
 // import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		paths: {
+			base: dev ? '' : '/greeencalendar2021',
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
