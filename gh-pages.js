c6cb918aa3535e1a8bef0ccae249cce140242d04
Svale Fossåskaret <svaleaf@gmail.com>
2021-12-01 10:03:42 +0100

    Update build files

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,9 @@ publish(
  'build', // path to public directory
  {
   branch: 'gh-pages',
-  repo: 'https://github.com/svale/greeencalendar2021.git', // Update to point to your repository
+  remote: 'staging',
+  repo: 'https://github.com/svale/greeencalendar2021.git',
+  // repo: 'https://github.com/netliferesearch/klodekalenderen',
   user: {
    name: 'Svale', // update to use your name
    email: 'svale.fossaskaret@netlife.com' // Update to use your email
