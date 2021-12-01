056495e14354cc2d45976c5ef8c948486951229c
Svale Fossåskaret <svaleaf@gmail.com>
2021-12-01 07:30:20 +0100

    Cleanup

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,6 @@
 <script>
-	import Header from '$lib/header/Header.svelte';
 	import '../app.css';
 </script>
-
-<!-- <Header /> -->
 
 <main>
 	<slot />
@@ -11,23 +8,7 @@
 
 <style>
 	main {
-		background-color: pink;
 		min-width: 100vw;
 		min-height: 100vh;
-		/* flex: 1;
-		display: flex;
-		flex-direction: column;
-		width: 100vw;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box; */
 	}
-
-	/* footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: stretch;
-		align-items: stretch;
-	} */
-
 </style>
